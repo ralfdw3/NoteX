cdeb301d57936d3ad5a1e5a3e89588f8b77881ca
Ralf Drehmer Wink <105680174+ralfdw3@users.noreply.github.com>
2023-07-20 09:37:49 -0300

    first commit

---
+++ src/main/java/com/notex/notes/NoteXApplication.java
@@ -0,0 +1,13 @@
+package com.notex.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notex/notes/NoteXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notex.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
